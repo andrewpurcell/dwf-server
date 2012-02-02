806c53bf2667f742a9c7886db4e547db5ea0603e
Andrew Purcell <andrew.purcell@tufts.edu>
2012-02-01 22:45:39 -0500

    merged from original repo

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem "mogli"
 gem "json"
 gem "httparty"
 gem "thin"
+gem 'redis', '2.2.2'
+gem 'erubis', '2.7.0'
--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ set :show_exceptions, false
 # permissions your app needs.
 # See https://developers.facebook.com/docs/reference/api/permissions/
 # for a full list of permissions
-FACEBOOK_SCOPE = 'user_likes,user_photos,user_photo_video_tags'
+FACEBOOK_SCOPE = 'read_friendlists,manage_friendlists,offline_access'
 
 unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_SECRET"]
   abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
@@ -46,6 +46,22 @@ helpers do
   def first_column(item, collection)
     return ' class="first-column"' if collection.index(item)%4 == 0
   end
+  def print_info_on_active(user)
+    "#{user["name"]}, #{user["email"]}"
+  end
+
+  def validate_post_contents(post)
+    username = params[:username]
+    ttl = params[:time]
+    if !User.exists? username
+      puts "user doesn't exist"
+      return false
+    elsif ttl.to_i <= 0
+      puts "bad time"
+      return false
+    end
+    true
+  end
 end
 
 # the facebook session expired! reset ours and restart the process
@@ -66,8 +82,6 @@ get "/" do
 
   # access friends, photos and likes directly through the user instance
   @friends = @user.friends[0, 4]
-  @photos  = @user.photos[0, 16]
-  @likes   = @user.likes[0, 4]
 
   # for other data you can always run fql
   @friends_using_app = @client.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
@@ -94,4 +108,79 @@ get '/auth/facebook/callback' do
   client = Mogli::Client.create_from_code_and_authenticator(params[:code], authenticator)
   session[:at] = client.access_token
   redirect '/'
+end
+
+
+configure :production do
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+configure :test, :development do
+  $redis = Redis.new
+end
+
+# set :views, File.join(File.dirname(__FILE__), '../views')
+# set :public_folder, File.join(File.dirname(__FILE__), '../public')
+set :dump_errors, true
+
+
+
+get '/' do 
+  @title = "DineWithFriends - social eating"
+  erb :index
+end
+
+post '/new_user' do 
+  name = params[:name]
+  uid = params[:username]
+  email = params[:email]
+  phone = params[:phone]
+
+  if User.exists?(uid)
+    body << "Username already exists"
+  else
+    User.create(name, uid, email, phone)
+    puts "new user: #{name}, #{uid}, #{email}, #{phone}"
+    body << "User created!"
+  end
+end
+
+get '/find_matches/:username' do
+  if User.exists?(params[:username])
+    @friends = User.find(params[:username]).get_active_friends()
+    if @friends.length > 0
+      erb :show_friends
+    else
+      erb :no_friends
+    end
+  else
+    erb :no_such_user
+  end
+end
+
+post '/post' do
+  if validate_post_contents(params)
+    user = User.find(params[:username])
+    user.create_event(params[:time]*60)
+    @name = params[:username]
+    erb :post_success
+  else
+    erb :post_failed
+  end
+end
+
+get '/active' do
+  @all_users = User.get_all_active_users()
+  puts @all_users.inspect
+  erb :all_active
+end
+
+get '/users/:username' do
+  begin
+    @user = User.find(params[:username])
+    erb :exists
+  rescue
+      erb :no_such_user
+    end
 end
